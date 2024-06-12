2d79fa0da99b42c70c79a605f115c725c59acc68
blue-script <alexm92@inbox.ru>
2024-06-12 15:49:18 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        mainColor: '#033E6B',
+      },
+      padding: {
+        big: '50px',
+      }
+    },
+  },
+  plugins: [],
+}
+
